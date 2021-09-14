25cac126e93a11f7c25bd02f2fae7dd657c3a249
Weverton procópio <wevertonprocopio690@gmail.com>
2021-09-14 20:49:21 -0300

    initial structure has been finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
+import { Hello } from './components/hello.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Hello
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/hello.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-hello',
+    templateUrl: './hello.component.html'
+})
+
+export class Hello {}

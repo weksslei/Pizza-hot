a7089ebb31ba247bfcb8e88da7f43b85a64371cd
Weverton <wevertonprocopio690@gmail.com>
2021-10-14 20:50:31 -0300

    subscription was added !

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,28 @@
 import { Data } from './../../models/data.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RepositoryService } from 'src/app/services/repository.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
   public data!: Data;
   constructor(private repositoryService: RepositoryService) { }
 
   ngOnInit(): void {
-     this.repositoryService.getJson().subscribe(data => this.data = data);
+    this.subscription.add(this.getJson());
+  }
+
+  ngOnDestroy():void {
+    this.subscription.unsubscribe()
+  }
+
+  private getJson() {
+    this.repositoryService.getJson().subscribe(data => this.data = data);
   }
 
 }

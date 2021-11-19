d1ad6b8feae91a84ea2c0bb2ee33ba44042ef36b
Vitor Barbosa <cefetcnita@gmail.com>
2021-11-18 23:10:56 -0300

    pai fez as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardsComponent } from './components/cards/cards.component';
+import { HomeComponent } from './components/home/home.component';
+import { OfertasComponent } from './components/ofertas/ofertas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'cards', component: CardsComponent
+  },
+  {
+    path: 'ofertas', component: OfertasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { HomeModule } from './components/home/home.module';
-import { NavigationModule } from './shared/components/navigation/navigation.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
